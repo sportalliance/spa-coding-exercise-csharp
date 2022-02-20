a223eaf16e91612c56f0dd5fbe35a473b92bf28f
Oliver Saggau <oliver.saggau@sysvision.de>
2022-02-20 15:32:14 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercise
+{
+    public class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
